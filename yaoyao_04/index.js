e1bcd793b3f80fb9adcb1ba66f0a5e40467ec2b0
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2017-12-14 01:26:24 +0800

    yaoyao_04 rewrite

--- yaoyao_04/index.js
+++ yaoyao_04/index.js
@@ -4,21 +4,11 @@ s.turnRightBtn = document.getElementById('turn-right');
 s.turnBackBtn = document.getElementById('turn-back');
 s.MoveBtn = document.getElementById('move');
 s.element = document.getElementById('square');
-s.face = 'top';
 s.site = [0, 0];
-s.faceIndex = '0';
-s.faceTab = ['top', 'right', 'down', 'left'];
+s.faceAngle = 0;
 
 s.render = function() {
-    if (s.face === 'top') {
-        s.element.style.transform = 'rotate(0deg)';
-    } else if (s.face === 'right') {
-        s.element.style.transform = 'rotate(90deg)';
-    } else if (s.face === 'down') {
-        s.element.style.transform = 'rotate(180deg)';
-    } else if (s.face === 'left') {
-        s.element.style.transform = 'rotate(-90deg)';
-    }
+    s.element.style.transform = `rotate(${s.faceAngle}deg)`;
     var hor = s.site[0] * 50 + 2;
     var ver = s.site[1] * 50 + 2;
     s.element.style.top = ver + 'px';
@@ -27,36 +17,47 @@ s.render = function() {
 
 s.init = function() {
     s.turnLeftBtn.onclick = function() {
-        s.face = 'left';
+        s.faceAngle -= 90;
         s.render();
     };
 
     s.turnRightBtn.onclick = function() {
-        s.face = 'right';
+        s.faceAngle += 90;
         s.render();
     };
 
     s.turnBackBtn.onclick = function() {
-        s.face = 'down';
+        s.faceAngle += 180;
         s.render();
     };
 
     s.MoveBtn.onclick = function() {
-        if (s.face === 'top') {
+        var angle;
+        if (s.faceAngle < 0) {
+            angle = 360 - Math.abs(s.faceAngle) % 360;
+        } else {
+            angle = s.faceAngle;
+        }
+
+        if (angle % 360 === 0) {
+            // up
             if (s.site[1] > 0) {
                 s.site[1] -= 1;
             }
-        } else if (s.face === 'right') {
-            if (s.site[0] < 9) {
-                s.site[0] += 1;
+        } else if (angle % 360 === 270) {
+            // left
+            if (s.site[0] > 0) {
+                s.site[0] -= 1;
             }
-        } else if (s.face === 'down') {
+        } else if (angle % 360 === 180) {
+            // down
             if (s.site[1] < 9) {
                 s.site[1] += 1;
             }
-        } else if (s.face === 'left') {
-            if (s.site[0] > 0) {
-                s.site[0] -= 1;
+        } else if (angle % 360 === 90) {
+            // right
+            if (s.site[0] < 9) {
+                s.site[0] += 1;
             }
         }
         s.render();
