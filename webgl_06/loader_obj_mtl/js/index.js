4d2a366af59e4a515b933a002fe5b924f394fab8
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2018-01-04 11:04:46 +0800

    webgl 6

---
+++ webgl_06/loader_obj_mtl/js/index.js
@@ -0,0 +1,82 @@
+var main = {
+    init: function(){
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true,
+        });
+        this.renderer.setClearColor(0x333333);
+        this.renderer.setSize(window.innerWidth,window.innerHeight);
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.canvas = this.renderer.domElement;
+        document.body.appendChild(this.canvas);
+        this.canvasHeight = this.canvas.clientHeight;
+        this.canvasWidth = this.canvas.clientWidth;        
+        this.scene  = new THREE.Scene();
+
+        this.spotlight = new THREE.SpotLight(0x999999, 1, 0, Math.PI / 3,2);
+        this.spotlight.position.set(0, 1000, 800);
+        this.spotlight2 = new THREE.SpotLight(0xffffff, 1, 5000, Math.PI / 6, 1,2);
+        this.spotlight2.position.set(0, 3000, 0);
+        this.spotlight2.castShadow = true;
+        this.spotlight2.shadow.mapSize.width = 2048;
+        this.spotlight2.shadow.mapSize.height = 2048;
+        this.spotlight2.shadow.camera.far = 3000;
+        this.amblight = new THREE.AmbientLight(0x222222);
+        this.camera = new THREE.PerspectiveCamera(45, 
+            this.canvasWidth / this.canvasHeight, 1, 20000);
+        this.camera.position.set(0, 200, 200);
+        this.control = new THREE.OrbitControls(this.camera); 
+        this.clock = new THREE.Clock();
+        this.scene.add(this.camera,  this.amblight,this.spotlight2,this.spotlight,);
+        this.addTreeMod();
+        this.addGround();
+        this.animation();
+    },
+    animation:function(){
+        requestAnimationFrame(this.animation.bind(this));
+        this.renderer.render(this.scene, this.camera);
+    },
+    resizeCanvas: function(){
+        
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.camera.aspect = window.innerWidth/window.innerHeight;
+        this.camera.updateProjectionMatrix();
+    },
+    addGround: function(){
+        var shape = new THREE.CubeGeometry(100, 2, 100);
+        var material = new THREE.MeshPhongMaterial({
+            color: 0x779966,
+            emissive: 0x333333,
+        });
+        ground = new THREE.Mesh(shape, material);
+        ground.receiveShadow = true;
+        this.scene.add(ground);
+        this.ground = ground;
+    },
+    addTreeMod: function(){
+        _this = this;
+        var mtlLoader = new THREE.MTLLoader();
+        var objLoader = new THREE.OBJLoader();
+        mtlLoader.load('model/tree/materials.mtl', function(material){
+            objLoader.load('model/tree/model.obj',function(object){
+                _this.tree = object;
+                var children = object.children;
+                for (var i = 0; i < children.length; i++) {
+                    children[i].castShadow= true;
+                    children[i].receiveShadow = true;
+                }
+                object.scale.set(100, 100, 100);
+                object.translateY(32);
+                _this.scene.add(object);
+            });
+            objLoader.setMaterials(material);
+        });
+    },
+};
+
+main.init();
+window.onresize = function(){
+    main.resizeCanvas();
+};
+
---
+++ webgl_06/loader_pmd_pmx_vmd/js/index.js
@@ -0,0 +1,107 @@
+var main = {
+    init: function(){
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true,
+        });
+        this.renderer.setClearColor('#FFF6EF');
+        this.renderer.setSize(window.innerWidth,window.innerHeight);
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        this.canvas = this.renderer.domElement;
+        document.body.appendChild(this.canvas);
+        this.canvasHeight = this.canvas.clientHeight;
+        this.canvasWidth = this.canvas.clientWidth;        
+        this.scene  = new THREE.Scene();
+
+        this.spotlight = new THREE.SpotLight(0x999999, 1, 0, Math.PI / 3,2);
+        this.spotlight.position.set(0, 1000, 800);
+        this.spotlight2 = new THREE.SpotLight(0xffffff, 1, 5000, Math.PI / 6, 1,2);
+        this.spotlight2.position.set(0, 3000, 0);
+        this.spotlight2.castShadow = true;
+        this.spotlight2.shadow.mapSize.width = 2048;
+        this.spotlight2.shadow.mapSize.height = 2048;
+        this.spotlight2.shadow.camera.far = 3000;
+        this.amblight = new THREE.AmbientLight(0x222222);
+        this.camera = new THREE.PerspectiveCamera(45, 
+            this.canvasWidth / this.canvasHeight, 1, 20000);
+        this.camera.position.set(0, 18, 13);
+
+        this.control = new THREE.OrbitControls(this.camera); 
+        this.clock = new THREE.Clock();
+        this.scene.add(this.camera,  this.amblight,this.spotlight2,this.spotlight,);
+        this.addMmdModer();
+        //this.addGround();
+        this.animation();
+    },
+    animation:function(){
+        if (this.ready && this.mmdHelper) {
+            this.mmdHelper.animate(this.clock.getDelta());
+        }
+        this.camera.lookAt(0, 16, 0);
+        requestAnimationFrame(this.animation.bind(this));
+        this.renderer.render(this.scene, this.camera);
+    },
+    resizeCanvas: function(){
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.camera.aspect = window.innerWidth/window.innerHeight;
+        this.camera.updateProjectionMatrix();
+    },
+    addGround: function(){
+        var shape = new THREE.CubeGeometry(100, 2, 100);
+        var material = new THREE.MeshPhongMaterial({
+            color: 0x779966,
+            emissive: 0x333333,
+        });
+        ground = new THREE.Mesh(shape, material);
+        ground.receiveShadow = true;
+        this.scene.add(ground);
+        this.ground = ground;
+    },
+    addMmdModer: function(){
+        _this = this;
+        var mmdLoader = new THREE.MMDLoader();
+        var mmdHelper = new THREE.MMDHelper();
+        var mmdFile = 'model/kizunaai/kizunaai.pmx';
+        var AudioFile = 'mp3/Ririri.mp3';
+        // 动作文件
+        var vmdFile = [
+            'model/vmd/Ririri.vmd'
+        ];
+        var audioParams = { delayTime: 0};
+        mmdLoader.load(mmdFile, vmdFile,function(object){
+            _this.ia = object;
+            object.castShadow = true;
+            object.position.y = 2;
+            
+            mmdHelper.setPhysics( object );
+            _this.mmdHelper = mmdHelper;
+            mmdHelper.add(object);
+            mmdHelper.setAnimation(object);
+            mmdLoader.loadAudio( AudioFile, function (audio, listener) {
+                listener.position.z = 1;
+                mmdHelper.setAudio(audio, listener, audioParams);
+                mmdHelper.unifyAnimationDuration();
+                _this.ready = true;
+                _this.scene.add(object, listener,audio);
+            });
+            //CCDIKHelper
+            // ikHelper = new THREE.CCDIKHelper(object);
+            // ikHelper.visible = true;
+            // _this.scene.add(ikHelper);
+            // _this.ikHelper = ikHelper;
+
+            //MMDPhysicsHelper
+            // physicsHelper = new THREE.MMDPhysicsHelper(object);
+            // physicsHelper.visible = false;
+            // _this.scene.add(physicsHelper);
+            // _this.physicsHelper = physicsHelper;         
+        });
+    },
+};
+
+main.init();
+window.onresize = function(){
+    main.resizeCanvas();
+};
+
